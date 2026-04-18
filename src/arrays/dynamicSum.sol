bcaefafa69edfbc5d0e591901c997f3449ac8e91
HOUUKIM <kimcoyg9753@gmail.com>
2026-04-19 01:49:37 +0900

    Create dynamicSum.sol

---
+++ src/arrays/dynamicSum.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract Contract {
+    function sum(uint[] calldata x) external pure returns(uint total) {
+        for(uint i=0; i<x.length; i++){
+            total += x[i];
+        }
+    }
+}
